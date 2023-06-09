e940cbaeb63ac32ed309078f09e04f28344c526f
German Samoluk <german.samoluk@gmail.com>
2023-06-09 09:01:54 -0300

    eliminated v1 from calculator operations

--- server.cjs
+++ server.cjs
@@ -63,113 +63,11 @@ app.post('/auth/v2/login/', function (req, res) {
 
 });
 
-const operationMap = {'addition':calculator.addition,
-						'subtraction':calculator.subtraction,
-						'multiplication':calculator.multiplication,
-						'division':calculator.division,
-						'random_string':calculator.randomString,
-						'square_root':calculator.squareRoot};
-
-function selectOneRow(database, query, params) {
-    return new Promise((resolve, reject) => {
-        database.get(query, params, (err, row) => {
-            if (err) {
-                reject(err); 
-            } else {
-				resolve(row);
-            }
-        });
-    });
-}
-
-function execStatement(database, statement, params) {
-    return new Promise((resolve, reject) => {
-        database.run(statement,params, (err) => {
-            if (err) {
-                reject(err); 
-            } else {
-				resolve("OK");
-            }
-        });
-    });
-}
 
 app.post('/calculator/v2/operations/:operation', function (req, res) {
 	calculator.execOperation(req)
 	.then(result => {res.status(result.httpCode).json(result)})
 })
-
-app.post('/calculator/v1/operations/:operation', function (req, res) {
-	try {		
-	var token = req.get('x-access-token');
-	  
-	var validToken = false;
-	
-	const decodedUsername = jwt.verify(token,process.env.TOKEN_KEY);
-	
-	let db = new sqlite3.Database('./db/calculator.db');
-
-	let sql = `SELECT balance FROM users where users.username = ?`;
-
-	var response = {}
-	var responseCode = {}
-
-	selectOneRow(db, sql, [decodedUsername.user_id])
-	.then(row => {
-		if (row !== undefined) {
-		  var oldBalance = row.balance;
-		  var firstOperand = req.body.firstOperand;
-		  var secondOperand = req.body.secondOperand;
-		  
-		  if (operationMap[req.params.operation] == undefined) {
-			return res.status(401).json({"resultCode":"-10","result":"Operation not supported","value":""});  
-		  } else {
-			  checkBalanceQuery = `select users.balance - operations.cost as new_balance from users,operations where users.username = '${decodedUsername.user_id}' and operations.type='${req.params.operation}'`;
-			  selectOneRow(db, checkBalanceQuery, [])
-			  .then(newBalance => {
-				  if (newBalance.new_balance >= 0) {
-					  operationMap[req.params.operation](firstOperand,secondOperand)
-					  .then(result => {if (result.success){
-						  execStatement(db,`update users set balance = ?`,newBalance.new_balance)
-						  execStatement(db,`insert into records (operation_id,user_id,amount,user_balance,operation_response,operation_date,active) 
-															select op.id as operation_id, 
-															u.id as user_id,
-															op.cost as amount,
-															u.balance as balance,
-															'OK' as operation_response,
-															datetime('now') as operation_date,
-															1 as active
-															from operations op, users u
-															where op.type = ?
-															and u.username = ?`,[req.params.operation,decodedUsername.user_id])
-							
-						  return res.status(200).json({"resultCode":"0","result":"OK","value":result.opResult,"balance":newBalance.new_balance});		  
-						} else {
-						  return res.status(401).json({"resultCode":"-11","result":"There was an error executing the operation","value":"","balance":oldBalance});
-					  }})
-						
-				  } else {
-					  return res.status(401).json({"resultCode":"-20","result":"The current balance is insufficient to execute the operation","value":"","newBalance":row.balance});
-				  }
-				  
-			  })
-			  
-		  }		
-		} else {
-			return res.status(403).json({"resultCode":"-2","result":"INVALID TOKEN","token":"","balance":0});			
-		}
-	})
-	.catch(error => {
-		console.log(error)
-		return res.status(500).json({"resultCode":"-15","result":"internal error"})
-	});
-		
-	} catch (err) {
-	  res.status(500).json({"resultCode":"-15","result":"internal error"})
-	}
-})
-
-
 
 app.get('/calculator/v1/history', function (req, res) {
 	calculator.getHistory(req)
